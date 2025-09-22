e1354e7b20f7da8fed7ea9f44fbfca3e3948e988
vedantkherade <152008775+vedantkherade@users.noreply.github.com>
2025-09-22 10:30:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
+const tr = document.createElement("tr");
+const td = document.createElement("td");
+const table = document.getElementById("tab");
+td.innerText = "Total Price";
+tr.appendChild(td);
+	
 const getSum = () => {
 //Add your code here
+	let priceList = document.querySelectorAll(".price");
+	const td = document.createElement("td");
+	let total = 0;
+
+	priceList.forEach((price) => {
+		total += parseInt(price.textContent, 10);
+		
+	});
+
+	td.setAttribute("id", "ans");
+	td.innerText = total;
+	tr.appendChild(td);
+	table.appendChild(tr);
   
 };
 
